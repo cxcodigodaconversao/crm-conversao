c83c1aae3357a6cb43d9bce0adeb10bc05bb44fb
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-10-27 19:59:53 -0300

    Create CRMPage component with Kanban integration

---
+++ app/(dashboard)/crm/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import Kanban from './components/Kanban';
+
+export default function CRMPage() {
+  return (
+    <div className="h-screen bg-gray-900">
+      <Kanban />
+    </div>
+  );
+}
